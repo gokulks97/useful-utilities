23bee9124d4982dedd38b693b2a5864f66db0b65
GokulKS <111052691+gokulks97@users.noreply.github.com>
2023-03-27 13:31:41 +0530

    added NOTE and reverted file_name variable to empty so user can provide user specificname

--- get_control_id_and_title.rb
+++ get_control_id_and_title.rb
@@ -1,8 +1,16 @@
+#NOTE:
+# this script will only work incase a profile has the translated controls in it.
+# DEBT : need to have logic to generate control id and title sheet without translated control.rb file itself.
+
+
 # need to have spreadsheet gem in local
 require "spreadsheet"
 # specify the location for file_name variable as per your repo path
 # it has to be, path of translated_control.rb which is having the most controls for that profile
-file_name = "/Users/gokulks/Documents/code_repo/chef_repos/compliance-profiles/src/inspec/supported/cis-oraclelinux7-level2-server-v3.1.1/controls/translated-controls.rb"
+# example_path "/Users/gokulks/Documents/code_repo/chef_repos/compliance-profiles/src/inspec/supported/cis-oraclelinux7-level2-server-v3.1.1/controls/translated-controls.rb"
+file_name = ""
+raise "Please update the variable file_name in the script with full path to translated control.rb as shown i
+n comments" if file_name.empty?
 output_file_name = "cont_id_title.xls"
 file_content = File.read(file_name)
 book = Spreadsheet::Workbook.new
@@ -15,7 +23,7 @@ if file_content.match? /xccdf_org.cisecurity.benchmarks_rule_/
     x = x.gsub("xccdf_org.cisecurity.benchmarks_rule_","")
     c_id = x.scan(/[\d.]+/)[0]
     title = x.scan(/(?<=_).*/)[0].gsub("_"," ")
-    sheet1.update_row row_count, c_id, title 
+    sheet1.update_row row_count, c_id, title
     row_count += 1
   end
 elsif file_content.match? /xccdf_mil.disa.stig_rule_/
